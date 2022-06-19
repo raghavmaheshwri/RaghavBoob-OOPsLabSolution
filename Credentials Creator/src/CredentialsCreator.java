12e666cb0f93c7839b375a773a9da3d0d4cfff5e
Raghav Boob <raghavmaheshwri@gmail.com>
2022-06-19 20:25:43 +0530

    Unwanted code removed

--- Credentials Creator/src/CredentialsCreator.java
+++ Credentials Creator/src/CredentialsCreator.java
@@ -21,18 +21,6 @@ public class CredentialsCreator {
 		lastName = lastName.strip();
 		System.out.println("-----");
 
-		// Condition to check First and last name is not empty
-		if (firstName == "") {
-			System.out.println("First Name Can't be empty");
-			System.out.print("Please Enter Employee First Name: ");
-			firstName = sc.nextLine();
-		}
-		if (lastName == "") {
-			System.out.println("Last Name Can't be empty");
-			System.out.print("Please Enter Employee Last Name: ");
-			lastName = sc.nextLine();
-			System.out.println("-----");
-		}
 
 		// Using parameterized constructor of class Employee to set first and last name
 		Employee empData = new Employee(firstName, lastName);
